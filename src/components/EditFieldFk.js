f6ca499e0c4cc9401708db365602d6db2b2603a8
sfhardma <s.f.hardman@massey.ac.nz>
2019-10-04 12:24:13 +1300

    test

--- src/components/EditFieldFk.js
+++ src/components/EditFieldFk.js
@@ -12,6 +12,9 @@ import Select from 'react-select';
  * @extends {React.Component<Props>}
  */
 class EditFieldFk extends React.Component {
+  /**
+   * @param {Props} props
+   */
   constructor(props) {
     super(props);
     this.valueChanged = this.valueChanged.bind(this);
